73ac8618235f7f9844af680fa54e66cba64adcd7
liuyongbo <berb11@163.com>
2020-07-27 16:58:34 +0800

    -

--- DevOps/Linux/shell/github01.sh
+++ DevOps/Linux/shell/github01.sh
@@ -2,23 +2,32 @@
 # 克隆或更新所有GitHub项目代码
 
 urlarr=(
-# Java
+    '
+    Java
     https://github.com/CyC2018/CS-Notes.git
     https://github.com/Snailclimb/JavaGuide.git
     https://github.com/linlinjava/litemall.git
-# Python
+    '
+    '
+    Python
     https://github.com/jackfrued/Python-100-Days.git
     https://github.com/TheAlgorithms/Python.git
-# Front
+    '
+    '
+    Front
     https://github.com/30-seconds/30-seconds-of-code.git
     https://github.com/bailicangdu/vue2-elm.git
-# DevOps
+    '
+    '
+    DevOps
     https://github.com/Micropoor/Micro8.git
     https://github.com/imthenachoman/How-To-Secure-A-Linux-Server.git
     https://github.com/trimstray/nginx-quick-reference.git
     https://github.com/toddmotto/public-apis.git
     https://github.com/trimstray/the-book-of-secret-knowledge.git
-# Deep
+    '
+    '
+    Deep
     https://github.com/tensorflow/tensorflow.git
     https://github.com/aymericdamien/TensorFlow-Examples.git
     https://github.com/jikexueyuanwiki/tensorflow-zh.git
@@ -33,39 +42,59 @@ urlarr=(
     https://github.com/apachecn/AiLearning.git
     https://github.com/deepfakes/faceswap.git
     https://github.com/imhuay/Algorithm_Interview_Notes-Chinese.git
-# Tool
+    '
+    '
+    Tool
     https://github.com/521xueweihan/git-tips.git
     https://github.com/geeeeeeeeek/git-recipes.git
     https://github.com/pomber/git-history.git
     https://github.com/jaywcjlove/awesome-mac.git
     https://github.com/zhaoolee/ChromeAppHeroes.git
     https://github.com/wklchris/Note-by-LaTeX.git
-# Blog
+    '
+    '
+    Blog
     https://github.com/kon9chunkit/GitHub-Chinese-Top-Charts.git
     https://github.com/timqian/chinese-independent-blogs.git
-# Interview
+    '
+    '
+    Interview
     https://github.com/0voice/interview_internal_reference.git
     https://github.com/CyC2018/CS-Notes.git
     https://github.com/azl397985856/leetcode.git
     https://github.com/MisterBooo/LeetCodeAnimation.git
     https://github.com/mbinary/USTC-CS-Courses-Resource.git
+    '
+
 
 )
 
-path= pwd
-
+path=`pwd`
+echo "path==== $path"
 clone_update(){
+    echo "$1===AAA====$2"
     if [ ! -d ${1} ];then # 目录是否存在
         git clone ${1}
     else
         cd $1
         git pull
-        cd $path
     fi
 }
 
 
-for k in ${urlarr[@]};do
-    echo $k
-    clone_update $k
+for i in "${urlarr[@]}" ; do
+    arritem=($i) #此时b就相当于二维数组里面的一维数组了，然后可以再次遍历
+    arrlen=`expr ${#arritem[@]} - 1`
+    for j in $(seq 1 $arrlen); do
+        diritem=${arritem[0]}
+        if [ ! -d ${diritem} ];then # 目录是否存在
+            mkdir $diritem
+        fi
+        if [ $j -eq 0 ]; then
+            continue
+        fi
+        cd $diritem
+        clone_update ${arritem[j]}
+        cd $path
+    done
 done
